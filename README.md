869987efcf82a60160f080f65bc4efb4874128b6
islam-Attar <islamattar95@gmail.com>
2022-03-26 22:16:03 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # class13socket.io
+
+# Desicrption
+* did an application called `class13socket.io` to contact client with each others using messages. I used socket.io and socket.io-client to link between server and clients.
+
+* Createed an html flie that contains a form which sends messages from the client(user) and joins to a room.
+
+* creating socket.io connection.
+
+* build the application using `socket.io`, `socket.io-clent`, `@socket.io/admin-ui`, and `snowpack`. They were used to fire events from the client to the server, also from the server to the clients. 
+
+* You can run it by running `node server.js` on server side and then `npm run start ` on client side and after that it will open a chrome or whatever default broswer your PC has, on port `8080` and from their you can open mutliple tabs and send messages in the message section, also you can join rooms to send private messages to same clients who are connected on the same room.
+
+# UML
+
+![](./lab13.png)
+
+
+# links
+
+- [pull request]()
---
+++ client/script.js
@@ -0,0 +1,59 @@
+import { io } from "socket.io-client"
+
+const joinRoomButton = document.getElementById("room-button");
+const messageInput = document.getElementById("message-input");
+const roomInput = document.getElementById("room-input");
+const form = document.getElementById("form");
+const socket = io('http://localhost:4444')
+const userSocket = io("http://localhost:4444/user", { auth: { token: 'islam' } })
+
+userSocket.on('connect_error', error => {
+    displayMessage(error)
+})
+
+socket.on('connect', () => {
+    displayMessage(`you are connected with ID: ${socket.id} `)
+})
+
+socket.on('recieved-message', (message) => {
+    displayMessage(message)
+})
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const message = messageInput.value
+    const room = roomInput.value
+    if (message === "") return
+    displayMessage(message)
+    socket.emit('send-message', message, room)
+
+    messageInput.value = ""
+
+})
+
+joinRoomButton.addEventListener("click", () => {
+    const room = roomInput.value;
+    socket.emit('join-room', room, message => {
+        displayMessage(message)
+    })
+})
+
+function displayMessage(message) {
+    const div = document.createElement("div")
+    div.textContent = message;
+    document.getElementById("message-container").append(div)
+
+}
+
+let count = 0;
+
+setInterval(() => {
+    socket.volatile.emit("ping", ++count)
+}, 2000);
+
+document.addEventListener("keydown", e => {
+    if (e.target.matches('input')) return
+    if (e.key === 'c') socket.connect()
+    if (e.key === 'd') socket.disconnect()
+
+})
